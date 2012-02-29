94241b4007582812f84f1a77e8a3f6ecc0bf0406
Kim Brugger <kim.brugger@easih.ac.uk>
2012-02-29 16:48:22 +0000

    extending the table definition, still some things to sort out.

--- sql/conductor.sql
+++ sql/conductor.sql
@@ -23,10 +23,44 @@ CREATE TABLE sample (
 );
 
 
+
+CREATE TABLE file (
+
+  fid                 INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  sid                 INT NOT NULL ,
+  rid		      INT NOT NULL,
+  name                VARCHAR(50) NOT NULL ,
+  timestamp           BIGINT,
+
+  KEY name_idx (name),
+  KEY sid_idx  (sid)
+);
+
+
+CREATE TABLE run (
+
+  rid                 INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  mid		      INT NOT NULL,
+  name                VARCHAR(100) NOT NULL UNIQUE,
+
+  KEY name_idx (name)
+);
+
+CREATE TABLE sequencer (
+
+  mid                 INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name                VARCHAR(100) NOT NULL UNIQUE,
+  platform            VARCHAR(50),
+  
+  KEY name_idx (name)
+);
+
+
 CREATE TABLE analysis (
   aid                INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   reference	     VARCHAR(100),
-  pipeline	     VARCHAR(100)
+  pipeline	     VARCHAR(100),
+  min_reads          INT
 );
 
 
